f754f8f02e1c8c5dbf934423579ec801c338726d
litepubl <yarrowsoft@gmail.com>
2017-05-27 00:33:27 +0300

    rc7.08 added event manager

--- src/Data.php
+++ src/Data.php
@@ -1,9 +1,10 @@
 <?php
 namespace litepubl\core\data;
 
-use litepubl\core\storage\Storable;
-use litepubl\core\storage\StorableTrait;
-use litepubl\core\storage\StorageInterface;
+use litepubl\core\storage\Data as DataStorage;
+use litepubl\core\container\factories\InstallableInterface;
+use litepubl\core\container\factories\InstallableTrait;
+use litepubl\core\container\factories\FactoryInterface;
 
 /**
  * This is the base class to storage data
@@ -14,36 +15,22 @@ use litepubl\core\storage\StorageInterface;
  * @property-read string $thisTable
  */
 
-class Data implements Storable
+class Data extends DataStorage implements InstallableInterface
 {
-    use StorableTrait;
+    use InstallableTrait;
     use PropsTrait;
     use DBTrait;
-    use InstallerTrait;
 
     const ZERODATE = '0000-00-00 00:00:00';
     public $table;
+    protected $factory;
 
-    public static function i()
+    public function __construct(FactoryInterface $factory)
     {
-        return static ::iGet(get_called_class());
-    }
-
-    public static function iGet(string $class)
-    {
-        return static ::getAppInstance()->classes->getInstance($class);
-    }
-
-    public static function getAppInstance(): App
-    {
-        return litepubl::$app;
-    }
-
-    public function __construct()
-    {
+        $this->factory = $factory;
         if (!$this->basename) {
-            $class = get_class($this);
-            $this->basename = substr($class, strrpos($class, '\\') + 1);
+            $className = get_class($this);
+            $this->baseName = substr($className, strrpos($className, '\\') + 1);
         }
 
         $this->create();
@@ -64,8 +51,13 @@ class Data implements Storable
         $this->free();
     }
 
-    public function free()
+    protected function free()
     {
+    }
+
+    public function getFactory(): FactoryInterface
+    {
+        return $this->factory;
     }
 
     public function error($Msg, $code = 0)
@@ -76,109 +68,5 @@ class Data implements Storable
     public function getApp(): App
     {
         return static ::getAppInstance();
-    }
-
-    public function install()
-    {
-        $this->externalchain('Install');
-    }
-
-    public function uninstall()
-    {
-        $this->externalchain('Uninstall');
-    }
-
-    public function validate($repair = false)
-    {
-        $this->externalchain('Validate', $repair);
-    }
-
-    protected function externalChain($func, $arg = null)
-    {
-        $parents = class_parents($this);
-        array_splice($parents, 0, 0, get_class($this));
-        foreach ($parents as $class) {
-            $this->externalFunc($class, $func, $arg);
-        }
-    }
-
-    public function getExternalFuncName($class, $func)
-    {
-        $reflector = new \ReflectionClass($class);
-        $filename = $reflector->getFileName();
-
-        if (strpos($filename, 'kernel.')) {
-            $filename = dirname($filename) . DIRECTORY_SEPARATOR . basename(str_replace('\\', DIRECTORY_SEPARATOR, $class)) . '.php';
-        }
-
-        $externalname = basename($filename, '.php') . '.install.php';
-        $dir = dirname($filename) . DIRECTORY_SEPARATOR;
-        $file = $dir . 'install' . DIRECTORY_SEPARATOR . $externalname;
-        if (!file_exists($file)) {
-            $file = $dir . $externalname;
-            if (!file_exists($file)) {
-                return false;
-            }
-        }
-
-        include_once $file;
-
-        $fnc = (is_object($class) ? get_class($class) : (string) $class) . $func;
-        if (function_exists($fnc)) {
-            return $fnc;
-        }
-
-        return false;
-    }
-
-    public function externalFunc($class, $func, $args)
-    {
-        if ($fnc = $this->getExternalFuncName($class, $func)) {
-            if (is_array($args)) {
-                array_unshift($args, $this);
-            } else {
-                $args = [
-                    $this,
-                    $args
-                ];
-            }
-
-            return \call_user_func_array($fnc, $args);
-        }
-    }
-
-    public function getStorage()
-    {
-        return $this->getApp()->storage;
-    }
-
-    public function getClass(): string
-    {
-        return get_class($this);
-    }
-
-    public function getDbversion()
-    {
-        return false;
-    }
-
-    public function getDb($table = '')
-    {
-        $table = $table ? $table : $this->table;
-        if ($table) {
-            $this->getApp()->db->table = $table;
-        }
-
-        return $this->getApp()->db;
-    }
-
-    protected function getThisTable()
-    {
-        return $this->getApp()->db->prefix . $this->table;
-    }
-
-    public static function getClassName($c): string
-    {
-        return is_object($c) ? get_class($c) : trim($c);
     }
 }
---
+++ src/Events.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace litepubl\core\data;
+
+use litepubl\core\container\factories\FactoryInterface;
+
+class Events extends Data
+{
+    const EVENTS = 'events';
+    protected $eventManager;
+
+    public function __construct(FactoryInterface $factory)
+    {
+        parent::__construct($factory);
+        $this->eventManager = $this->factory->createEvents();
+        $this->load();
+    }
+
+    public function getData(): array
+    {
+        $result = $this->data;
+        $result[static::EVENTS] = $this->eventManager->getData();
+        return $result;
+    }
+
+    public function setData(array $data)
+    {
+        $this->eventManager->setData($data);
+        unset($data[static::EVENTS]);
+        $this->data = $data;
+    }
+}
